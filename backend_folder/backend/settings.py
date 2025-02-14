198c887d7b0309bda92a83430f8a703bb3a41dbb
Juan Navarlaz Urtubia <juannavarlaz.jn@gmail.com>
2025-02-14 19:15:14 +0100

    Changes to avoid releasing information that compromises the system.

--- backend_folder/backend/settings.py
+++ backend_folder/backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_qc2-50lf!)z1idpuh5$(^xh&)lo*btgf4b799f!ve!m49nv=x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
