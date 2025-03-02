ba6b0ed8a667441b937b0d6a6c69b3822329b6e1
Juan Navarlaz Urtubia <juannavarlaz.jn@gmail.com>
2025-03-02 10:21:49 +0100

    changes in authentication payload

--- backend_folder/api/views.py
+++ backend_folder/api/views.py
@@ -150,8 +150,6 @@ def login_view(request):
     user_data = {
         'alias': user.alias,
         'email': user.email,
-        'first_name': user.first_name,
-        'last_name': user.last_name,
         'access_token': access_token,
         'refresh_token': refresh_token,
         'role': None,
@@ -175,7 +173,7 @@ def verify_view(request):
     if data is None:
         return Response({"message": "User not logged in"}, status=status.HTTP_401_UNAUTHORIZED)
     
-    required_fields = ['alias', 'email', 'first_name', 'last_name', 'access_token', 'refresh_token', 'role']
+    required_fields = ['alias', 'email', 'access_token', 'refresh_token', 'role']
     for field in required_fields:
         if field not in data:
             return Response({"message": f"Missing field: {field}"}, status=status.HTTP_400_BAD_REQUEST)
