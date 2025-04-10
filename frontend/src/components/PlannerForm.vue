04dd30a990c9d3276e264b34dad383ac653a7d77
Juan Navarlaz Urtubia <juannavarlaz.jn@gmail.com>
2025-04-10 12:53:48 +0200

    frontend plan generator integration almost complete.

--- frontend/src/components/PlannerForm.vue
+++ frontend/src/components/PlannerForm.vue
@@ -9,7 +9,8 @@
           <div class="search-container">
             <!-- Buscador de ciudades -->
             <v-text-field v-model="cityQuery" label="Buscar Ciudad" placeholder="Introduce una ciudad"
-              @input="handleCityInput" outlined dense @focus="onFocus" @blur="onBlurCity" class="no-margin"></v-text-field>
+              @input="handleCityInput" outlined dense @focus="onFocus" @blur="onBlurCity"
+              class="no-margin"></v-text-field>
 
             <!-- Lista de sugerencias de ciudad -->
             <transition name="slide-fade">
@@ -23,19 +24,15 @@
           </div>
 
           <!-- Campos de fecha de inicio y fin del viaje -->
-          <v-text-field v-model="startDate" label="Fecha de Inicio" type="date" outlined dense 
-            :min="today" @change="handleStartDateChange" @blur="validateStartDate"></v-text-field>
-          <v-text-field v-model="endDate" label="Fecha de Fin" type="date" outlined dense 
-            :min="startDate" :disabled="!startDate" @blur="validateEndDate"></v-text-field>
+          <v-text-field v-model="startDate" label="Fecha de Inicio" type="date" outlined dense :min="today"
+            @change="handleStartDateChange" @blur="validateStartDate"></v-text-field>
+          <v-text-field v-model="endDate" label="Fecha de Fin" type="date" outlined dense :min="startDate"
+            :disabled="!startDate" @blur="validateEndDate"></v-text-field>
 
           <!-- Menú desplegable de categorías -->
           <v-select v-model="selectedCategories" :items="categories" label="Categorías de los lugares a recomendar"
             placeholder="Selecciona una o varias categorías" outlined dense multiple chips></v-select>
 
-          <!-- Checkboxes para hoteles y restaurantes -->
-          <v-checkbox v-model="includeHotels" label="Hoteles" dense></v-checkbox>
-
-          <v-checkbox v-model="includeRestaurants" label="Restaurantes" dense></v-checkbox>
 
           <!-- Botón de envío -->
           <v-btn color="primary" block @click="submitForm">Buscar</v-btn>
@@ -43,6 +40,10 @@
       </v-col>
     </v-row>
   </v-container>
+  <div class="loading" id="loading" hidden>
+    <div class="spin" hidden></div>
+    <div class="loading-text" hidden>Cargando...</div>
+  </div>
 </template>
 
 <script>
@@ -53,8 +54,6 @@ export default {
       citySuggestions: [],
       selectedCategories: [],
       categories: [], // Dinámico
-      includeHotels: false,
-      includeRestaurants: false,
       debounceTimeout: null,
       cache: {}, // Almacenar consultas previas
       isCityListVisible: false, // Controla la visibilidad de la lista de sugerencias
@@ -115,7 +114,7 @@ export default {
       }
     },
 
-    submitForm() {
+    async submitForm() {
       const missingFields = [];
       if (!this.cityQuery) missingFields.push('Buscar Ciudad');
       if (!this.startDate) missingFields.push('Fecha de Inicio');
@@ -129,15 +128,40 @@ export default {
       const searchData = {
         city: this.cityQuery,
         categories: this.selectedCategories,
-        includeHotels: this.includeHotels,
-        includeRestaurants: this.includeRestaurants,
         startDate: this.startDate,
         endDate: this.endDate,
       };
       console.log("Datos enviados: ", searchData);
       localStorage.setItem('travel_data', JSON.stringify(searchData));
       localStorage.setItem('form_state', JSON.stringify(searchData));
-      window.location.reload(); // Recargar la página para mostrar los resultados
+      document.getElementById('loading').hidden = false; // Mostrar el loader
+      document.getElementsByClassName('spin')[0].hidden = false; // Mostrar el spinner
+      document.getElementsByClassName('loading-text')[0].hidden = false; // Mostrar el texto de carga
+      try {
+        const response = await fetch('http://127.0.0.1:8000/plan/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(searchData),
+        });
+        document.getElementById('loading').hidden = true; // Ocultar el loader
+        document.getElementsByClassName('spin')[0].hidden = true; // Ocultar el spinner
+        document.getElementsByClassName('loading-text')[0].hidden = true; // Ocultar el texto de carga
+        if (!response.ok) {
+          alert("No se encontraron resultados para tu búsqueda.");
+          return;
+        }
+
+        const travelPlan = await response.json(); // Procesar el cuerpo de la respuesta como JSON
+        console.log("Respuesta del servidor: ", travelPlan);
+        localStorage.setItem('travel_data', JSON.stringify(travelPlan)); // Guardar el JSON en localStorage
+        window.location.reload(); // Recargar la página para mostrar el plan de viaje
+      } catch (error) {
+        console.error("Error al obtener el plan de viaje: ", error);
+        alert("Hubo un error al procesar tu solicitud. Por favor, inténtalo de nuevo.");
+      }
+
     },
 
     restoreFormState() {
@@ -146,8 +170,6 @@ export default {
         const state = JSON.parse(formState);
         this.cityQuery = state.city;
         this.selectedCategories = state.categories;
-        this.includeHotels = state.includeHotels;
-        this.includeRestaurants = state.includeRestaurants;
         this.startDate = state.startDate;
         this.endDate = state.endDate;
       }
@@ -170,9 +192,9 @@ export default {
     validateStartDate() {
       const todayParts = this.today.split('-');
       const startDateParts = this.startDate.split('-');
-      if (startDateParts[0] < todayParts[0] || 
-          (startDateParts[0] === todayParts[0] && startDateParts[1] < todayParts[1]) || 
-          (startDateParts[0] === todayParts[0] && startDateParts[1] === todayParts[1] && startDateParts[2] < todayParts[2])) {
+      if (startDateParts[0] < todayParts[0] ||
+        (startDateParts[0] === todayParts[0] && startDateParts[1] < todayParts[1]) ||
+        (startDateParts[0] === todayParts[0] && startDateParts[1] === todayParts[1] && startDateParts[2] < todayParts[2])) {
         this.startDate = this.today;
       }
       this.endDate = ''; // Borrar la fecha de fin si se modifica la fecha de inicio
@@ -181,9 +203,9 @@ export default {
     validateEndDate() {
       const startDateParts = this.startDate.split('-');
       const endDateParts = this.endDate.split('-');
-      if (endDateParts[0] < startDateParts[0] || 
-          (endDateParts[0] === startDateParts[0] && endDateParts[1] < startDateParts[1]) || 
-          (endDateParts[0] === startDateParts[0] && endDateParts[1] === startDateParts[1] && endDateParts[2] < startDateParts[2])) {
+      if (endDateParts[0] < startDateParts[0] ||
+        (endDateParts[0] === startDateParts[0] && endDateParts[1] < startDateParts[1]) ||
+        (endDateParts[0] === startDateParts[0] && endDateParts[1] === startDateParts[1] && endDateParts[2] < startDateParts[2])) {
         this.endDate = this.startDate;
       }
     }
@@ -203,20 +225,27 @@ export default {
 
 /* Estilo para las sugerencias */
 .city-suggestions {
-  border: 1px solid #ccc; /* Borde de la lista */
-  border-top: none; /* No mostrar borde superior, ya que se conecta al campo de búsqueda */
-  border-radius: 0 0 8px 8px; /* Bordes redondeados inferiores */
+  border: 1px solid #ccc;
+  /* Borde de la lista */
+  border-top: none;
+  /* No mostrar borde superior, ya que se conecta al campo de búsqueda */
+  border-radius: 0 0 8px 8px;
+  /* Bordes redondeados inferiores */
   max-height: 200px;
-  overflow-y: auto; /* Hacer la lista desplazable si tiene muchas opciones */
+  overflow-y: auto;
+  /* Hacer la lista desplazable si tiene muchas opciones */
   width: 100%;
   top: 100%;
   margin-top: 0 !important;
   padding-top: 0 !important;
   margin-bottom: 2em;
   left: 0;
-  z-index: 10; /* Asegura que esté encima de otros componentes */
-  background-color: white; /* Fondo blanco para las sugerencias */
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Sombras para un efecto elevado */
+  z-index: 10;
+  /* Asegura que esté encima de otros componentes */
+  background-color: white;
+  /* Fondo blanco para las sugerencias */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  /* Sombras para un efecto elevado */
 }
 
 /* Asegura que el campo de búsqueda y las sugerencias estén pegados */
@@ -248,5 +277,55 @@ export default {
 .no-margin .v-input__control {
   margin-bottom: 0 !important;
   padding-bottom: 0 !important;
+}
+
+#loading {
+  hidden: true;
+  /* Ocultar por defecto */
+}
+
+.loading {
+  display: flex;
+  flex-direction: column;
+  /* Apilar los elementos verticalmente */
+  justify-content: center;
+  /* Centrar los elementos verticalmente dentro del contenedor */
+  align-items: center;
+  /* Centrar los elementos horizontalmente */
+  width: 100%;
+  /* Asegurar que ocupe todo el ancho disponible */
+  max-width: 400px;
+  /* Limitar el ancho máximo del contenedor */
+  margin: 0 auto;
+  /* Centrar horizontalmente el contenedor */
+  padding: 20px;
+  /* Espaciado interno */
+  border-radius: 8px;
+  /* Bordes redondeados */
+}
+
+.spin {
+  border: 3px solid rgba(60, 239, 255, 0.2);
+  /* Color adaptado */
+  border-top-color: #3cefff;
+  /* Color principal */
+  border-radius: 50%;
+  width: 3em;
+  height: 3em;
+  animation: spin 1s linear infinite;
+}
+
+.loading-text {
+  margin-top: 1em;
+  font-size: 1.2em;
+  color: #333;
+  text-align: center;
+  /* Centrar el texto */
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
