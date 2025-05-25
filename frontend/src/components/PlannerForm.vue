95786c54117edb350883631291af04a09fd19f1d
Juan Navarlaz Urtubia <jnavarlaz002@ikasle.ehu.eus>
2025-05-25 08:58:48 +0200

    Update in planner form view, corrected city suggestion list interaction to fill the city text field.

--- frontend/src/components/PlannerForm.vue
+++ frontend/src/components/PlannerForm.vue
@@ -8,15 +8,31 @@
           <!-- Buscador de ciudades y lista de sugerencias -->
           <div class="search-container">
             <!-- Buscador de ciudades -->
-            <v-text-field v-model="cityQuery" label="Buscar Ciudad" placeholder="Introduce una ciudad"
-              @input="handleCityInput" outlined dense @focus="onFocus" @blur="onBlurCity"
-              class="no-margin"></v-text-field>
+            <v-text-field
+              v-model="cityQuery"
+              label="Buscar Ciudad"
+              placeholder="Introduce una ciudad"
+              @input="handleCityInput"
+              outlined
+              dense
+              @focus="onFocus"
+              @blur="onBlurCity"
+              class="no-margin"
+            ></v-text-field>
 
             <!-- Lista de sugerencias de ciudad -->
             <transition name="slide-fade">
-              <v-list v-if="citySuggestions.length && isCityListVisible" class="city-suggestions" dense>
-                <v-list-item v-for="(city, index) in citySuggestions" :key="index" @click="selectCity(city)"
-                  class="cursor-pointer">
+              <v-list
+                v-if="citySuggestions.length && isCityListVisible"
+                class="city-suggestions"
+                dense
+              >
+                <v-list-item
+                  v-for="(city, index) in citySuggestions"
+                  :key="index"
+                  @mousedown="selectCity(city)"
+                  class="cursor-pointer"
+                >
                   <v-list-item-title>{{ city }}</v-list-item-title>
                 </v-list-item>
               </v-list>
@@ -24,15 +40,38 @@
           </div>
 
           <!-- Campos de fecha de inicio y fin del viaje -->
-          <v-text-field v-model="startDate" label="Fecha de Inicio" type="date" outlined dense :min="today"
-            @change="handleStartDateChange" @blur="validateStartDate"></v-text-field>
-          <v-text-field v-model="endDate" label="Fecha de Fin" type="date" outlined dense :min="startDate"
-            :disabled="!startDate" @blur="validateEndDate"></v-text-field>
+          <v-text-field
+            v-model="startDate"
+            label="Fecha de Inicio"
+            type="date"
+            outlined
+            dense
+            :min="today"
+            @change="handleStartDateChange"
+            @blur="validateStartDate"
+          ></v-text-field>
+          <v-text-field
+            v-model="endDate"
+            label="Fecha de Fin"
+            type="date"
+            outlined
+            dense
+            :min="startDate"
+            :disabled="!startDate"
+            @blur="validateEndDate"
+          ></v-text-field>
 
           <!-- Menú desplegable de categorías -->
-          <v-select v-model="selectedCategories" :items="categories" label="Categorías de los lugares a recomendar"
-            placeholder="Selecciona una o varias categorías" outlined dense multiple chips></v-select>
-
+          <v-select
+            v-model="selectedCategories"
+            :items="categories"
+            label="Categorías de los lugares a recomendar"
+            placeholder="Selecciona una o varias categorías"
+            outlined
+            dense
+            multiple
+            chips
+          ></v-select>
 
           <!-- Botón de envío -->
           <v-btn color="primary" block @click="submitForm">Buscar</v-btn>
@@ -50,16 +89,16 @@
 export default {
   data() {
     return {
-      cityQuery: '',
+      cityQuery: "",
       citySuggestions: [],
       selectedCategories: [],
       categories: [], // Dinámico
       debounceTimeout: null,
       cache: {}, // Almacenar consultas previas
       isCityListVisible: false, // Controla la visibilidad de la lista de sugerencias
-      startDate: '',
-      endDate: '',
-      today: new Date().toISOString().split('T')[0], // Fecha actual en formato YYYY-MM-DD
+      startDate: "",
+      endDate: "",
+      today: new Date().toISOString().split("T")[0], // Fecha actual en formato YYYY-MM-DD
     };
   },
   mounted() {
@@ -70,15 +109,14 @@ export default {
   },
   methods: {
     check_seen() {
-      const seen1 = localStorage.getItem('seen');
-        if (seen1) {
-        
+      const seen1 = localStorage.getItem("seen");
+      if (seen1) {
         if (seen1 === "0") {
-          localStorage.setItem('seen', "1");
+          localStorage.setItem("seen", "1");
         }
         if (seen1 === "1") {
-          localStorage.removeItem('plan_pdf');
-          localStorage.removeItem('seen');
+          localStorage.removeItem("plan_pdf");
+          localStorage.removeItem("seen");
         }
       }
     },
@@ -88,11 +126,13 @@ export default {
         return;
       }
 
-      const endpoint = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(this.cityQuery)}&format=json&limit=5`;
+      const endpoint = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
+        this.cityQuery
+      )}&format=json&limit=5`;
       try {
         const response = await fetch(endpoint);
         const data = await response.json();
-        const suggestions = data.map(location => location.display_name);
+        const suggestions = data.map((location) => location.display_name);
         this.citySuggestions = suggestions;
         this.cache[this.cityQuery] = suggestions; // Cachear la respuesta
       } catch (error) {
@@ -100,27 +140,27 @@ export default {
       }
     },
     async checkUserData() {
-      const storedUserData = localStorage.getItem('user_data');
+      const storedUserData = localStorage.getItem("user_data");
       if (storedUserData) {
         const userData = JSON.parse(storedUserData);
 
         try {
-          const response = await fetch('http://localhost:8000/verify/', {
-            method: 'POST',
+          const response = await fetch("http://localhost:8000/verify/", {
+            method: "POST",
             headers: {
-              'Content-Type': 'application/json',
+              "Content-Type": "application/json",
             },
             body: JSON.stringify({ user_data: userData }),
           });
 
           if (!response.ok) {
-            throw new Error('Verification failed');
+            throw new Error("Verification failed");
           }
 
-          console.log('User data is valid.');
+          console.log("User data is valid.");
         } catch (error) {
-          console.error('Invalid user data. Removing from localStorage.');
-          localStorage.removeItem('user_data');
+          console.error("Invalid user data. Removing from localStorage.");
+          localStorage.removeItem("user_data");
         }
       }
     },
@@ -128,12 +168,16 @@ export default {
     async submitForm() {
       await this.checkUserData(); // Verificar user_data antes de ejecutar el formulario
       const missingFields = [];
-      if (!this.cityQuery) missingFields.push('Buscar Ciudad');
-      if (!this.startDate) missingFields.push('Fecha de Inicio');
-      if (!this.endDate) missingFields.push('Fecha de Fin');
+      if (!this.cityQuery) missingFields.push("Buscar Ciudad");
+      if (!this.startDate) missingFields.push("Fecha de Inicio");
+      if (!this.endDate) missingFields.push("Fecha de Fin");
 
       if (missingFields.length > 0) {
-        alert(`Por favor, rellena los siguientes campos: ${missingFields.join(', ')}`);
+        alert(
+          `Por favor, rellena los siguientes campos: ${missingFields.join(
+            ", "
+          )}`
+        );
         return;
       }
 
@@ -145,43 +189,45 @@ export default {
       };
 
       // Agregar user_data si está presente en localStorage
-      const storedUserData = localStorage.getItem('user_data');
+      const storedUserData = localStorage.getItem("user_data");
       if (storedUserData) {
         searchData.user_data = JSON.parse(storedUserData);
       }
 
-      console.log('Datos enviados: ', searchData);
-      localStorage.setItem('travel_data', JSON.stringify(searchData));
-      localStorage.setItem('form_state', JSON.stringify(searchData));
-      document.getElementById('loading').hidden = false; // Mostrar el loader
-      document.getElementsByClassName('spin')[0].hidden = false; // Mostrar el spinner
-      document.getElementsByClassName('loading-text')[0].hidden = false; // Mostrar el texto de carga
+      console.log("Datos enviados: ", searchData);
+      localStorage.setItem("travel_data", JSON.stringify(searchData));
+      localStorage.setItem("form_state", JSON.stringify(searchData));
+      document.getElementById("loading").hidden = false; // Mostrar el loader
+      document.getElementsByClassName("spin")[0].hidden = false; // Mostrar el spinner
+      document.getElementsByClassName("loading-text")[0].hidden = false; // Mostrar el texto de carga
       try {
-        const response = await fetch('http://127.0.0.1:8000/plan/', {
-          method: 'POST',
+        const response = await fetch("http://127.0.0.1:8000/plan/", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(searchData),
         });
-        document.getElementById('loading').hidden = true; // Ocultar el loader
-        document.getElementsByClassName('spin')[0].hidden = true; // Ocultar el spinner
-        document.getElementsByClassName('loading-text')[0].hidden = true; // Ocultar el texto de carga
+        document.getElementById("loading").hidden = true; // Ocultar el loader
+        document.getElementsByClassName("spin")[0].hidden = true; // Ocultar el spinner
+        document.getElementsByClassName("loading-text")[0].hidden = true; // Ocultar el texto de carga
 
         const travelPlan = await response.json(); // Procesar el cuerpo de la respuesta como JSON
-        console.log('Respuesta del servidor: ', travelPlan);
-        localStorage.setItem('travel_data', JSON.stringify(travelPlan)); // Guardar el JSON en localStorage
-        localStorage.setItem('plan_pdf', JSON.stringify(travelPlan)); // Guardar el PDF en localStorage
-        localStorage.setItem('seen', "0"); // Guardar el estado del formulario
+        console.log("Respuesta del servidor: ", travelPlan);
+        localStorage.setItem("travel_data", JSON.stringify(travelPlan)); // Guardar el JSON en localStorage
+        localStorage.setItem("plan_pdf", JSON.stringify(travelPlan)); // Guardar el PDF en localStorage
+        localStorage.setItem("seen", "0"); // Guardar el estado del formulario
         window.location.reload(); // Recargar la página para mostrar el plan de viaje
       } catch (error) {
-        console.error('Error al obtener el plan de viaje: ', error);
-        alert('Hubo un error al procesar tu solicitud. Por favor, inténtalo de nuevo.');
+        console.error("Error al obtener el plan de viaje: ", error);
+        alert(
+          "Hubo un error al procesar tu solicitud. Por favor, inténtalo de nuevo."
+        );
       }
     },
 
     restoreFormState() {
-      const formState = localStorage.getItem('form_state');
+      const formState = localStorage.getItem("form_state");
       if (formState) {
         const state = JSON.parse(formState);
         this.cityQuery = state.city;
@@ -193,11 +239,11 @@ export default {
 
     async fetchCategories() {
       try {
-        const response = await fetch('http://127.0.0.1:8000/getCategories/');
+        const response = await fetch("http://127.0.0.1:8000/getCategories/");
         const data = await response.json();
         this.categories = data.map((category) => category.name);
       } catch (error) {
-        console.error('Error fetching categories: ', error);
+        console.error("Error fetching categories: ", error);
       }
     },
 
@@ -226,40 +272,62 @@ export default {
     },
 
     selectCity(city) {
-      this.cityQuery = city;
-      this.isCityListVisible = false;
+      this.cityQuery = "";
+      this.$nextTick(() => {
+        console.log("Selected city:", city);
+        this.cityQuery = city;
+        this.isCityListVisible = false;
+        const input = this.$el.querySelector("input");
+        console.log("input:", input);
+        if (input) input.blur();
+      });
     },
 
     handleStartDateChange() {
       // Lógica para manejar el cambio de la fecha de inicio
-      const todayParts = this.today.split('-');
-      const startDateParts = this.startDate.split('-');
-      if (startDateParts[0] < todayParts[0] || 
-          (startDateParts[0] === todayParts[0] && startDateParts[1] < todayParts[1]) || 
-          (startDateParts[0] === todayParts[0] && startDateParts[1] === todayParts[1] && startDateParts[2] < todayParts[2])) {
+      const todayParts = this.today.split("-");
+      const startDateParts = this.startDate.split("-");
+      if (
+        startDateParts[0] < todayParts[0] ||
+        (startDateParts[0] === todayParts[0] &&
+          startDateParts[1] < todayParts[1]) ||
+        (startDateParts[0] === todayParts[0] &&
+          startDateParts[1] === todayParts[1] &&
+          startDateParts[2] < todayParts[2])
+      ) {
         this.startDate = this.today;
       }
-      this.endDate = '';
+      this.endDate = "";
     },
 
     validateStartDate() {
       // Validar la fecha de inicio que no sea anterior a la fecha actual
-      const todayParts = this.today.split('-');
-      const startDateParts = this.startDate.split('-');
-      if (startDateParts[0] < todayParts[0] || 
-          (startDateParts[0] === todayParts[0] && startDateParts[1] < todayParts[1]) || 
-          (startDateParts[0] === todayParts[0] && startDateParts[1] === todayParts[1] && startDateParts[2] < todayParts[2])) {
+      const todayParts = this.today.split("-");
+      const startDateParts = this.startDate.split("-");
+      if (
+        startDateParts[0] < todayParts[0] ||
+        (startDateParts[0] === todayParts[0] &&
+          startDateParts[1] < todayParts[1]) ||
+        (startDateParts[0] === todayParts[0] &&
+          startDateParts[1] === todayParts[1] &&
+          startDateParts[2] < todayParts[2])
+      ) {
         this.startDate = this.today;
       }
-      this.endDate = '';
+      this.endDate = "";
     },
 
     validateEndDate() {
-      const startDateParts = this.startDate.split('-');
-      const endDateParts = this.endDate.split('-');
-      if (endDateParts[0] < startDateParts[0] || 
-          (endDateParts[0] === startDateParts[0] && endDateParts[1] < startDateParts[1]) || 
-          (endDateParts[0] === startDateParts[0] && endDateParts[1] === startDateParts[1] && endDateParts[2] < startDateParts[2])) {
+      const startDateParts = this.startDate.split("-");
+      const endDateParts = this.endDate.split("-");
+      if (
+        endDateParts[0] < startDateParts[0] ||
+        (endDateParts[0] === startDateParts[0] &&
+          endDateParts[1] < startDateParts[1]) ||
+        (endDateParts[0] === startDateParts[0] &&
+          endDateParts[1] === startDateParts[1] &&
+          endDateParts[2] < startDateParts[2])
+      ) {
         this.endDate = this.startDate;
       }
     },
@@ -321,8 +389,7 @@ export default {
 .slide-fade-enter,
 .slide-fade-leave-to
 
-/* .slide-fade-leave-active en versiones anteriores de Vue */
-  {
+/* .slide-fade-leave-active en versiones anteriores de Vue */ {
   transform: translateY(-10px);
   opacity: 0;
 }
